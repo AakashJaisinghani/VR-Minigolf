38ca5c99446518f8f9102c990ab63769838ef650
Ashwin George <ashmartin855@gmail.com>
2025-08-14 20:19:30 -0400

    Ash push

--- web/studentD/studentD.js
+++ web/studentD/studentD.js
@@ -8,6 +8,31 @@ async function setupStudentD(scene) {
   let n = makeNodeObject(scene); // n holds meshes, empties/transformNodes, cameras and lights
   let a = makeAnimationGroupObject(scene);
 
+  (async () => {
+    const audioEngine = await BABYLON.CreateAudioEngineAsync();
+    await audioEngine.unlockAsync();
+})();
+
+let music = await BABYLON.CreateSoundAsync("music", "studentD/sound/bgmusic.mp3");
+music.volume = 0.4;
+music.play({ loop: true });
+
+const quack = await BABYLON.CreateSoundAsync("quack", "studentD/sound/quack.mp3");
+
+addPickAction(n.golfBall, function(item) {
+  var impulseDirection = scene.activeCamera.getForwardRay().direction;
+  let impulseMagnitude = 0.15;
+  let contactLocalRefPoint = BABYLON.Vector3.Zero();
+  item.physicsAggregate.body.applyImpulse(
+      impulseDirection.scale(impulseMagnitude),
+      item.getAbsolutePosition().add(contactLocalRefPoint)
+  );
+  quack.play();
+});
+
+
+
+
 
 
 } // DO NOT ERASE THIS
