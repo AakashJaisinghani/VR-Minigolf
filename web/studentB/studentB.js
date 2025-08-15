cebc2cf44b8aeae71f3614419927fdcdf9751d8f
vanisden <vani13591@gmail.com>
2025-08-14 20:12:51 -0400

    student B environment updated

--- web/studentB/studentB.js
+++ web/studentB/studentB.js
@@ -2,12 +2,23 @@ async function setupStudentB(scene) {
 
   // This is how you add models from a file:
   // await BABYLON.SceneLoader.AppendAsync("folder-name-if-file-in-folder", "filename.gltf");
-  // await BABYLON.SceneLoader.AppendAsync("", "studentB/studentB.glb");
+  await BABYLON.SceneLoader.AppendAsync("", "studentB/studentB.glb");
 
   // make sure these appear AFTER loading the GLB it refers to
   let n = makeNodeObject(scene); // n holds meshes, empties/transformNodes, cameras and lights
   let a = makeAnimationGroupObject(scene);
 
+
+  var dome = new BABYLON.PhotoDome(
+        "testdome",
+        "skybox360/Final360.png",
+        {
+            resolution: 32,
+            size: 1000
+        },
+        scene
+    );
+//dome.infiniteDistance = true;
 
 
 } // DO NOT ERASE THIS
